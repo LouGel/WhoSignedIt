97a425742b6e5cdd03c64d1843013d22b262fc0c
Louis Gelinet <nigelbook@Louiss-MacBook-Pro-2.local>
2025-03-28 13:07:00 +0100

    refacto: move impl command in approriated files

--- src/client/verify.rs
+++ src/client/verify.rs
@@ -1,4 +1,14 @@
+use std::path::Path;
+
 use clap::Args;
+
+use crate::{
+    error::AppError,
+    services::{
+        proof::{error::ProofErrorKind, ProofClientFactory},
+        signature::SignatureClientFactory,
+    },
+};
 
 use super::client::FormatInput;
 #[derive(Args, Debug, Clone)]
@@ -12,4 +22,23 @@ pub struct VerifyArgs {
 
     #[clap(short, long, default_value = "json")]
     pub format: FormatInput,
+}
+
+pub fn verify_proof(args: VerifyArgs) -> Result<(), AppError> {
+    let proof_str = if Path::new(args.proof.as_str()).exists() {
+        std::fs::read_to_string(args.proof.as_str()).unwrap()
+    } else {
+        args.proof.clone()
+    };
+    let signature_client = SignatureClientFactory::create_client(&args.blockchain)?;
+    let proof_client = ProofClientFactory::create_client(&args.proof, signature_client)?;
+
+    let proof = proof_client.from_str(&proof_str, args.format)?;
+    println!("Proof valid");
+    if proof.verify()? {
+        println!("Valid proof");
+        Ok(())
+    } else {
+        Err(AppError::ProofError(ProofErrorKind::Invalid))
+    }
 }
