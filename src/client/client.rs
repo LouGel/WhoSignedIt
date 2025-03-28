e9587d95050e6cb498a3bab6294804de65a791d3
Louis Gelinet <nigelbook@Louiss-MacBook-Pro-2.local>
2025-03-28 13:07:00 +0100

    refacto: move impl command in approriated files

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,13 +1,9 @@
-use super::{generate::GenerateArgs, verify::VerifyArgs};
-use crate::{
-    error::AppError,
-    services::{
-        proof::{error::ProofErrorKind, ProofClient, ProofClientFactory},
-        signature::{SignatureClient, SignatureClientFactory},
-    },
+use super::{
+    generate::{generate_proof, GenerateArgs},
+    verify::VerifyArgs,
 };
+use crate::{client::verify::verify_proof, error::AppError};
 use clap::{Parser, Subcommand};
-use std::{fs::File, io::Write, path::Path};
 
 #[derive(Debug, Clone, Copy)]
 pub enum FormatInput {
@@ -23,14 +19,6 @@ impl std::str::FromStr for FormatInput {
             other => Err(AppError::Input(format!("Invalid format {other}"))),
         }
     }
-}
-/// Main client for the application
-pub struct AppClient {
-    signature_client: Box<dyn SignatureClient>,
-    proof_client: Box<dyn ProofClient>,
-    command: Command,
-    output: Option<String>,
-    format: FormatInput,
 }
 
 /// CLI Arguments for the application
@@ -53,74 +41,12 @@ pub enum Command {
     Verify(VerifyArgs),
 }
 
+pub struct AppClient {}
 impl AppClient {
-    /// Create a new app client
-
-    /// Run the application
     pub fn run(args: &AppArgs) -> Result<(), AppError> {
         match args.command.clone() {
-            Command::Generate(sign_args) => {
-                let signature_client =
-                    SignatureClientFactory::create_client(&sign_args.blockchain)?;
-
-                // Parse or create a signature
-                let signature = if let Some(sig_str) = &sign_args.signature {
-                    // Parse an existing signature
-                    signature_client.from_str(sig_str)?
-                } else if let Some(private_key) = &sign_args.private_key {
-                    // Create a new signature
-                    signature_client.sign_message(&sign_args.message, private_key)?
-                } else {
-                    return Err(AppError::Custom(format!(
-                        "Either signature or private key must be provided"
-                    )));
-                };
-
-                // Parse the group public keys
-                let mut group = Vec::new();
-                for pubkey_str in &sign_args.group_public_keys {
-                    let pubkey = signature_client.parse_public_key(pubkey_str)?;
-                    group.push(pubkey);
-                }
-                let proof_client =
-                    ProofClientFactory::create_client(&sign_args.proof_type, signature_client)?;
-                // Create the group signature proof
-                let proof = proof_client.create_group_signature_proof(
-                    &sign_args.message,
-                    &signature,
-                    &group,
-                )?;
-                let proof_json = proof.format(&sign_args.format);
-                if let Some(output) = sign_args.output.as_ref() {
-                    let mut handle =
-                        File::create(output).expect("Cannot create of open output file");
-                    handle.write(proof_json.as_bytes()).unwrap();
-                } else {
-                    println!("{proof_json}")
-                }
-
-                Ok(())
-            }
-            Command::Verify(verify_args) => {
-                let proof_str = if Path::new(verify_args.proof.as_str()).exists() {
-                    std::fs::read_to_string(verify_args.proof.as_str()).unwrap()
-                } else {
-                    verify_args.proof.clone()
-                };
-                let signature_client =
-                    SignatureClientFactory::create_client(&verify_args.blockchain)?;
-                let proof_client =
-                    ProofClientFactory::create_client(&verify_args.proof, signature_client)?;
-
-                let proof = proof_client.from_str(&proof_str, verify_args.format)?;
-                println!("Proof valid");
-                if proof.verify()? {
-                    println!("Valid proof");
-                    Ok(())
-                } else {
-                    Err(AppError::ProofError(ProofErrorKind::Invalid))
-                }
-            }
+            Command::Generate(sign_args) => generate_proof(sign_args),
+            Command::Verify(verify_args) => verify_proof(verify_args),
             #[allow(unreachable_patterns)]
             _ => todo!("Verify command not implemented"),
         }
--- src/client/generate.rs
+++ src/client/generate.rs
@@ -1,4 +1,11 @@
+use std::fs::File;
+use std::io::Write;
+
 use clap::Args;
+
+use crate::error::AppError;
+use crate::services::proof::ProofClientFactory;
+use crate::services::signature::SignatureClientFactory;
 
 use super::client::FormatInput;
 #[derive(Args, Debug, Clone)]
@@ -34,4 +41,40 @@ pub struct GenerateArgs {
     /// Group public keys (comma-separated)
     #[clap(short, long, num_args = 1..)]
     pub group_public_keys: Vec<String>,
+}
+
+pub fn generate_proof(args: GenerateArgs) -> Result<(), AppError> {
+    let signature_client = SignatureClientFactory::create_client(&args.blockchain)?;
+
+    // Parse or create a signature
+    let signature = if let Some(sig_str) = &args.signature {
+        // Parse an existing signature
+        signature_client.from_str(sig_str)?
+    } else if let Some(private_key) = &args.private_key {
+        // Create a new signature
+        signature_client.sign_message(&args.message, private_key)?
+    } else {
+        return Err(AppError::Custom(format!(
+            "Either signature or private key must be provided"
+        )));
+    };
+
+    // Parse the group public keys
+    let mut group = Vec::new();
+    for pubkey_str in &args.group_public_keys {
+        let pubkey = signature_client.parse_public_key(pubkey_str)?;
+        group.push(pubkey);
+    }
+    let proof_client = ProofClientFactory::create_client(&args.proof_type, signature_client)?;
+    // Create the group signature proof
+    let proof = proof_client.create_group_signature_proof(&args.message, &signature, &group)?;
+    let proof_json = proof.format(&args.format);
+    if let Some(output) = args.output.as_ref() {
+        let mut handle = File::create(output).expect("Cannot create of open output file");
+        handle.write(proof_json.as_bytes()).unwrap();
+    } else {
+        println!("{proof_json}")
+    }
+
+    Ok(())
 }
